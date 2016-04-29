0ebc38c97e3ece78d86e1f4df6b56cb49b41622a
kayo <osako.k.ab@titech.ac.jp>
2016-04-29 16:34:43 +0900

    add log.d

--- app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
@@ -4,26 +4,54 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private TextView levelView;
     private HilbertView hilbertView;
     private final static int MAX_N = 9;
     private int n = 1;
+    private static final String TAG ="MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
         levelView = (TextView)findViewById(R.id.level_view);
         hilbertView = (HilbertView)findViewById(R.id.hilbert_view);
     }
-
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
     @Override
     protected void onResume() {
         super.onResume();
-        display();
+        Log.d(TAG, "onResume");
     }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    //@Override
+    //protected void onResume() {
+      //  super.onResume();
+       // display();
+    //}
 
     public void onClickDec(View v) {
         if (n > 1) n--;
